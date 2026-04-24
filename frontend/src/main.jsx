513ab479e70a9e1e1547ff41628da61f34374d59
r17e8h <riteshkr.rana18@gmail.com>
2026-04-24 22:28:02 +0530

    fix: final fix :[

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { CartProvider } from './context/CartContext'
 import { AuthProvider } from './context/AuthContext.jsx';
 import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
+axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
