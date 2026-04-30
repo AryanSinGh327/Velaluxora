8fe3f3fa3199c312caf9713653d5f484f75fb356
r17e8h <riteshkr.rana18@gmail.com>
2026-05-01 02:37:19 +0530

    Fix firebase config for vercel

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { envConfig } from "./config.js";
 
 const firebaseConfig = {
-  apiKey: envConfig.FIREBASE_API_KEY,
-  authDomain: envConfig.FIREBASE_AUTH_DOMAIN,
-  projectId: envConfig.FIREBASE_PROJECT_ID,
-  storageBucket: envConfig.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: envConfig.FIREBASE_MESSAGING_SENDER_ID,
-  appId: envConfig.FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
